bd8a3e971ffa7798289973d23d380f5a4532bcc8
fast-facts <107660271+fast-facts@users.noreply.github.com>
2024-09-16 18:29:27 -0400

    chore: Updated eslint config formatting

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,20 @@ const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const tsPlugin = require('@typescript-eslint/eslint-plugin');
 const angularPlugin = require('angular-eslint');
-const rxjs = require("eslint-plugin-rxjs-updated");
-const rxjsAngular = require("eslint-plugin-rxjs-angular-updated");
+const rxjs = require('eslint-plugin-rxjs-updated');
+const rxjsAngular = require('eslint-plugin-rxjs-angular-updated');
 
 module.exports = tseslint.config(
   {
     ignores: [
-      "**/node_modules/",
-      "**/e2e/",
-      "src/environments/",
-      "eslint.config.js"
+      '**/node_modules/',
+      '**/e2e/',
+      'src/environments/',
+      'eslint.config.js'
     ],
   },
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
 
     extends: [
       eslint.configs.recommended,
@@ -27,49 +27,49 @@ module.exports = tseslint.config(
 
     languageOptions: {
       ecmaVersion: 5,
-      sourceType: "script",
+      sourceType: 'script',
 
       parserOptions: {
         project: [
-          "projects/ngx-socketio2/tsconfig.lib.json",
-          "projects/ngx-socketio2/tsconfig.lib.prod.json",
-          "projects/ngx-socketio2/tsconfig.spec.json",
+          'projects/ngx-socketio2/tsconfig.lib.json',
+          'projects/ngx-socketio2/tsconfig.lib.prod.json',
+          'projects/ngx-socketio2/tsconfig.spec.json',
         ],
       },
     },
 
     plugins: {
-      "@typescript-eslint": tsPlugin,
+      '@typescript-eslint': tsPlugin,
       rxjs,
-      "rxjs-angular": rxjsAngular,
+      'rxjs-angular': rxjsAngular,
     },
 
     rules: {
-      eqeqeq: ["error", "always", {
-        null: "ignore",
+      eqeqeq: ['error', 'always', {
+        null: 'ignore',
       }],
-      "no-return-await": "error",
-      "prefer-arrow-callback": "error",
-      "quote-props": ["error", "as-needed"],
-      semi: ["error", "always"],
+      'no-return-await': 'error',
+      'prefer-arrow-callback': 'error',
+      'quote-props': ['error', 'as-needed'],
+      semi: ['error', 'always'],
 
-      "@typescript-eslint/no-deprecated": "error",
-      "@typescript-eslint/no-empty": "off",
-      "@typescript-eslint/no-empty-function": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-expressions": "off",
-      "@typescript-eslint/no-unused-vars": "off",
+      '@typescript-eslint/no-deprecated': 'error',
+      '@typescript-eslint/no-empty': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
 
-      "rxjs/no-nested-subscribe": "off",
-      "rxjs/no-unsafe-catch": "error",
-      "rxjs/no-unsafe-switchmap": "error",
-      "rxjs/no-unsafe-takeuntil": "error",
+      'rxjs/no-nested-subscribe': 'off',
+      'rxjs/no-unsafe-catch': 'error',
+      'rxjs/no-unsafe-switchmap': 'error',
+      'rxjs/no-unsafe-takeuntil': 'error',
 
-      "rxjs-angular/prefer-takeuntil": "error",
+      'rxjs-angular/prefer-takeuntil': 'error',
     },
   },
   {
-    files: ["**/*.html"],
+    files: ['**/*.html'],
 
     extends: [
       ...angularPlugin.configs.templateRecommended,
